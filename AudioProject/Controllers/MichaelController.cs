8c285cb92675c86821de4d0e62d5ca561681d92c
Michael Watts <wattsjmichael@gmail.com>
2020-11-02 16:39:01 -0800

    2 seperate Visualizers

---
+++ AudioProject/Controllers/MichaelController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using AudioProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System;
+
+namespace AudioProject.Controllers
+{
+  public class MichaelController : Controller
+  {
+    private readonly AudioProjectContext _db;
+
+    public MichaelController(AudioProjectContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View();
+    }
+    public ActionResult Upload()
+    {
+      return View();
+    }
+  }
+}
